300abbbc40259b282e7eaaf4048a1b0407b94512
Cusick1 <ryan.cusick.1997@gmail.com>
2025-06-11 00:10:53 -0400

    added url_launcher and implemented it for linkedIn and GitHub links

--- lib/presentation/about_page.dart
+++ lib/presentation/about_page.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:portfolio/constants.dart';
 import 'package:portfolio/presentation/widgets/contact_info.dart';
 
 class About extends StatelessWidget {
   const About({super.key});
+
+  Future<void> _launchUrl(String url) async {
+    final Uri uri = Uri.parse(url);
+    if (!await launchUrl(uri, mode: LaunchMode.externalApplication)) {
+      throw Exception('Could not launch $url');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,9 +24,16 @@ class About extends StatelessWidget {
           SizedBox(height: 12),
           Padding(padding: const EdgeInsets.all(8.0), child: ContactInfo()),
           Padding(
-            padding: const EdgeInsets.all(16.0),
+            padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
             child: Text(
-              Constants.summary,
+              Constants.summaryPart1,
+              style: TextStyle(fontSize: 20, color: Colors.black),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+            child: Text(
+              Constants.summaryPart2,
               style: TextStyle(fontSize: 15, color: Colors.black),
             ),
           ),
@@ -26,13 +41,25 @@ class About extends StatelessWidget {
             padding: const EdgeInsets.all(8.0),
             child: Column(
               children: [
-                Text(
-                  Constants.linkedInLink,
-                  style: TextStyle(color: Colors.black),
+                InkWell(
+                  onTap: () => _launchUrl(Constants.linkedInLink),
+                  child: Text(
+                    Constants.linkedInLink,
+                    style: TextStyle(
+                      color: Colors.blue[900],
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
                 ),
-                Text(
-                  Constants.gitHubLink,
-                  style: TextStyle(color: Colors.black),
+                InkWell(
+                  onTap: () => _launchUrl(Constants.gitHubLink),
+                  child: Text(
+                    Constants.gitHubLink,
+                    style: TextStyle(
+                      color: Colors.blue[900],
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
                 ),
               ],
             ),
