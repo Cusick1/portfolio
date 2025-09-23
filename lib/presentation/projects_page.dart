0943837dc1e67f378efa5d003e359caaa1f4c1bb
Ryan Cusick <38022300+Cusick1@users.noreply.github.com>
2025-09-22 23:26:13 -0400

    added project placeholders on projects page

--- lib/presentation/projects_page.dart
+++ lib/presentation/projects_page.dart
@@ -11,10 +11,97 @@ class Projects extends StatelessWidget {
     return Scaffold(
       backgroundColor: colorScheme.surface,
       body: Center(
-        child: Column(
+        child: ListView(
           children: [
             SizedBox(height: 12),
             Padding(padding: const EdgeInsets.all(8.0), child: ContactInfo()),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    children: [
+                      CircleAvatar(radius: 80, child: Text('1')),
+                      SizedBox(height: 8),
+                      Text(
+                        'More coming soon!',
+                        style: theme.textTheme.bodyLarge,
+                      ),
+                    ],
+                  ),
+                  SizedBox(width: 36),
+                  Column(
+                    children: [
+                      CircleAvatar(radius: 80, child: Text('2')),
+                      SizedBox(height: 8),
+                      Text(
+                        'More coming soon!',
+                        style: theme.textTheme.bodyLarge,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    children: [
+                      CircleAvatar(radius: 80, child: Text('3')),
+                      SizedBox(height: 8),
+                      Text(
+                        'More coming soon!',
+                        style: theme.textTheme.bodyLarge,
+                      ),
+                    ],
+                  ),
+                  SizedBox(width: 36),
+                  Column(
+                    children: [
+                      CircleAvatar(radius: 80, child: Text('4')),
+                      SizedBox(height: 8),
+                      Text(
+                        'More coming soon!',
+                        style: theme.textTheme.bodyLarge,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    children: [
+                      CircleAvatar(radius: 80, child: Text('5')),
+                      SizedBox(height: 8),
+                      Text(
+                        'More coming soon!',
+                        style: theme.textTheme.bodyLarge,
+                      ),
+                    ],
+                  ),
+                  SizedBox(width: 36),
+                  Column(
+                    children: [
+                      CircleAvatar(radius: 80, child: Text('6')),
+                      SizedBox(height: 8),
+                      Text(
+                        'More coming soon!',
+                        style: theme.textTheme.bodyLarge,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
